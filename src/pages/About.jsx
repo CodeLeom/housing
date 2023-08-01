09aaf5b8f4b31c3db02c12c51b38a18f80ff1b98
Ayodele Aransiola <leomdevs@gmail.com>
2023-08-01 12:35:52 +0100

    chore: added pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+
+
+function About() {
+  return (
+    <>
+      <h1 className="ml-10 text-3xl font-bold underline">
+           About Page
+          </h1>
+    </>
+  )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+
+function Home() {
+  return (
+    <>
+      <h1 className="ml-10 text-3xl font-bold underline">
+           Home Page
+          </h1>
+    </>
+  )
+}
+
+export default Home
