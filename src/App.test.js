a3436ca8fe059820838896b8eb51675f721c0c45
Ayodele Aransiola <leomdevs@gmail.com>
2023-08-04 20:42:30 +0100

    test: update test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('display featured house', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Featured House/i);
   expect(linkElement).toBeInTheDocument();
 });
